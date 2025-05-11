6a2eccb5cfc76fed7f4a02bfc80c16f607deeed1
Siddhi Raney <164665691+SiddhiRaney@users.noreply.github.com>
2025-05-11 22:00:21 +0530

    Update testing_the_model.py

--- testing_the_model.py
+++ testing_the_model.py
@@ -1,15 +1,17 @@
 import random
-
-# Pick a random test image
-idx = random.randint(0, len(X_test))
-img = X_test[idx]
-
-# Predict
-pred = model.predict(np.expand_dims(img, axis=0))
-pred_label = labels[np.argmax(pred)]
-
-# Show image and prediction
+import numpy as np
 import matplotlib.pyplot as plt
-plt.imshow(img)
-plt.title(f"Predicted: {pred_label}")
+
+# Select a random image from the test set
+random_index = random.randint(0, len(X_test) - 1)
+test_image = X_test[random_index]
+
+# Make prediction
+prediction = model.predict(np.expand_dims(test_image, axis=0), verbose=0)
+predicted_label = labels[np.argmax(prediction)]
+
+# Display image with prediction
+plt.imshow(test_image)
+plt.title(f"Predicted: {predicted_label}")
+plt.axis('off')  # Hide axes for cleaner display
 plt.show()
