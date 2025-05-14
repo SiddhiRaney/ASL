1fd68435325f37302c95e9a7ff6923e2745dd4b3
Siddhi Raney <164665691+SiddhiRaney@users.noreply.github.com>
2025-05-14 23:07:04 +0530

    Update testing_the_model.py

--- testing_the_model.py
+++ testing_the_model.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Randomly select one test image
-test_image = X_test[random.randint(0, len(X_test) - 1)]
+test_image = random.choice(X_test)
 
 # Predict the label
-pred = model.predict(test_image[None, ...], verbose=0)
+pred = model.predict(np.expand_dims(test_image, axis=0))
 predicted_label = labels[np.argmax(pred)]
 
 # Display the image and predicted label
-plt.imshow(test_image)
+plt.imshow(test_image.squeeze())  # Use .squeeze() if image has a singleton channel dimension
 plt.title(f"Predicted: {predicted_label}")
 plt.axis('off')
 plt.show()
